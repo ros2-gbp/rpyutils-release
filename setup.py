017c3b73667013dbad69337c2595e76ef72aea24
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-09-17 15:18:51 +0200

    Rebase from 'debian/humble/rpyutils'

--- setup.py
+++ setup.py
@@ -5,22 +5,21 @@ package_name = 'rpyutils'
 
 setup(
     name=package_name,
-    version='0.6.0',
+    version='0.2.2',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
     ],
-    maintainer='Dharini Dutia',
-    maintainer_email='dharini@openrobotics.org',
+    maintainer='Jacob Perron',
+    maintainer_email='jacob@openrobotics.org',
     url='https://github.com/ros2/rpyutils',
     download_url='https://github.com/ros2/rpyutils/releases',
     keywords=[],
     classifiers=[
         'Environment :: Console',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
     ],
     description='Python utilities for the ROS 2 code base.',
@@ -28,5 +27,9 @@ setup(
         'This package provides types and functions for common operations '
         'throughout the ROS 2 code base.'),
     license='Apache License, Version 2.0',
-    tests_require=['pytest'],
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
 )
